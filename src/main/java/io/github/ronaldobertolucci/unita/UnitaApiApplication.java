50e2ef3196a3d5ed8478ce93aae155634c274535
Ronaldo Bertolucci Jr <ronaldobertoluccijr@gmail.com>
2026-05-04 22:28:39 -0300

    fix: Sao Paulo time zone

--- src/main/java/io/github/ronaldobertolucci/unita/UnitaApiApplication.java
+++ src/main/java/io/github/ronaldobertolucci/unita/UnitaApiApplication.java
@@ -1,13 +1,21 @@
 package io.github.ronaldobertolucci.unita;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class UnitaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnitaApiApplication.class, args);
+	}
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
